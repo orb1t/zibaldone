6c57a7d74e6536551e60d35cbe332138ebd4afc5
Sam Halliday <sam.halliday@gmail.com>
2012-05-30 17:31:03 +0000

    UI redesign

--- src/main/java/uk/me/fommil/zibaldone/desktop/Mainscreen.java
+++ src/main/java/uk/me/fommil/zibaldone/desktop/Mainscreen.java
@@ -38,6 +38,7 @@ public class Mainscreen extends javax.swing.JFrame {
     public Mainscreen(ObservableGraph<Note, Double> graph) {
         initComponents();
 
+        jSettingsPanel.setVisible(false);
         controller = new JungMainController(graph);
     }
 
@@ -49,80 +50,104 @@ public class Mainscreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jToolBar = new javax.swing.JToolBar();
-        jButtonRefresh = new javax.swing.JButton();
-        jButtonClusters = new javax.swing.JButton();
-        jSearch = new org.jdesktop.swingx.JXSearchField();
-        jCloudButton = new javax.swing.JButton();
-        jSeparator1 = new javax.swing.JToolBar.Separator();
-        jButtonSynonyms = new javax.swing.JButton();
-        jButtonSources = new javax.swing.JButton();
         jFauxPanel = new javax.swing.JPanel();
-        jImportersPanel = new javax.swing.JPanel();
+        jToolBar = new javax.swing.JToolBar();
+        jSearch = new org.jdesktop.swingx.JXSearchField();
+        jCloudButton = new javax.swing.JToggleButton();
+        jButtonClusters = new javax.swing.JToggleButton();
+        jSeparator1 = new javax.swing.JToolBar.Separator();
+        jButtonSources = new javax.swing.JToggleButton();
+        jSettingsPanel = new javax.swing.JPanel();
+        jSettingsTabs = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jToolBar.setFloatable(false);
-        jToolBar.setRollover(true);
-        jToolBar.setBorderPainted(false);
-
-        jButtonRefresh.setText("Refresh");
-        jButtonRefresh.setFocusable(false);
-        jButtonRefresh.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButtonRefresh.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jToolBar.add(jButtonRefresh);
-
-        jButtonClusters.setText("Clusters");
-        jButtonClusters.setFocusable(false);
-        jButtonClusters.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButtonClusters.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jToolBar.add(jButtonClusters);
-        jToolBar.add(jSearch);
-
-        jCloudButton.setText("Tags");
-        jToolBar.add(jCloudButton);
-
-        jSeparator1.setPreferredSize(new java.awt.Dimension(100, 1));
-        jToolBar.add(jSeparator1);
-
-        jButtonSynonyms.setText("Synonyms");
-        jToolBar.add(jButtonSynonyms);
-
-        jButtonSources.setText("Importers");
-        jButtonSources.setFocusable(false);
-        jButtonSources.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButtonSources.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jButtonSources.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButtonSourcesMouseClicked(evt);
-            }
-        });
-        jToolBar.add(jButtonSources);
-
         jFauxPanel.setBackground(new java.awt.Color(0, 0, 0));
-
-        javax.swing.GroupLayout jImportersPanelLayout = new javax.swing.GroupLayout(jImportersPanel);
-        jImportersPanel.setLayout(jImportersPanelLayout);
-        jImportersPanelLayout.setHorizontalGroup(
-            jImportersPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 228, Short.MAX_VALUE)
-        );
-        jImportersPanelLayout.setVerticalGroup(
-            jImportersPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 415, Short.MAX_VALUE)
-        );
 
         javax.swing.GroupLayout jFauxPanelLayout = new javax.swing.GroupLayout(jFauxPanel);
         jFauxPanel.setLayout(jFauxPanelLayout);
         jFauxPanelLayout.setHorizontalGroup(
             jFauxPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jFauxPanelLayout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jImportersPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGap(0, 670, Short.MAX_VALUE)
         );
         jFauxPanelLayout.setVerticalGroup(
             jFauxPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jImportersPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 463, Short.MAX_VALUE)
+        );
+
+        jToolBar.setFloatable(false);
+        jToolBar.setRollover(true);
+        jToolBar.setBorderPainted(false);
+        jToolBar.add(jSearch);
+
+        jCloudButton.setText("Tags");
+        jToolBar.add(jCloudButton);
+
+        jButtonClusters.setText("Clusters");
+        jToolBar.add(jButtonClusters);
+
+        jSeparator1.setPreferredSize(new java.awt.Dimension(90, 1));
+        jToolBar.add(jSeparator1);
+
+        jButtonSources.setText("Settings");
+        jButtonSources.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jButtonSourcesStateChanged(evt);
+            }
+        });
+        jToolBar.add(jButtonSources);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 305, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 417, Short.MAX_VALUE)
+        );
+
+        jSettingsTabs.addTab("Importers", jPanel1);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 305, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 417, Short.MAX_VALUE)
+        );
+
+        jSettingsTabs.addTab("Synonyms", jPanel2);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 305, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 417, Short.MAX_VALUE)
+        );
+
+        jSettingsTabs.addTab("Advanced", jPanel3);
+
+        javax.swing.GroupLayout jSettingsPanelLayout = new javax.swing.GroupLayout(jSettingsPanel);
+        jSettingsPanel.setLayout(jSettingsPanelLayout);
+        jSettingsPanelLayout.setHorizontalGroup(
+            jSettingsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSettingsTabs, javax.swing.GroupLayout.Alignment.TRAILING)
+        );
+        jSettingsPanelLayout.setVerticalGroup(
+            jSettingsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSettingsTabs)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -131,6 +156,10 @@ public class Mainscreen extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jToolBar, javax.swing.GroupLayout.DEFAULT_SIZE, 670, Short.MAX_VALUE)
             .addComponent(jFauxPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGap(0, 344, Short.MAX_VALUE)
+                    .addComponent(jSettingsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -138,14 +167,18 @@ public class Mainscreen extends javax.swing.JFrame {
                 .addComponent(jToolBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jFauxPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGap(0, 44, Short.MAX_VALUE)
+                    .addComponent(jSettingsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButtonSourcesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonSourcesMouseClicked
-        jImportersPanel.setVisible(!jImportersPanel.isVisible());
-    }//GEN-LAST:event_jButtonSourcesMouseClicked
+    private void jButtonSourcesStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jButtonSourcesStateChanged
+        jSettingsPanel.setVisible(jButtonSources.isSelected());
+    }//GEN-LAST:event_jButtonSourcesStateChanged
 
     /** @param args */
     public static void main(String args[]) {
@@ -161,15 +194,17 @@ public class Mainscreen extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonClusters;
-    private javax.swing.JButton jButtonRefresh;
-    private javax.swing.JButton jButtonSources;
-    private javax.swing.JButton jButtonSynonyms;
-    private javax.swing.JButton jCloudButton;
+    private javax.swing.JToggleButton jButtonClusters;
+    private javax.swing.JToggleButton jButtonSources;
+    private javax.swing.JToggleButton jCloudButton;
     private javax.swing.JPanel jFauxPanel;
-    private javax.swing.JPanel jImportersPanel;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private org.jdesktop.swingx.JXSearchField jSearch;
     private javax.swing.JToolBar.Separator jSeparator1;
+    private javax.swing.JPanel jSettingsPanel;
+    private javax.swing.JTabbedPane jSettingsTabs;
     private javax.swing.JToolBar jToolBar;
     // End of variables declaration//GEN-END:variables
 }
