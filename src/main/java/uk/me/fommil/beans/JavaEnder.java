3687cef112198a29f32706f66ec41cd4d25295ca
Sam Halliday <sam.halliday@gmail.com>
2012-06-01 15:38:35 +0000

    table for editing text fields

--- src/main/java/uk/me/fommil/beans/JavaEnder.java
+++ src/main/java/uk/me/fommil/beans/JavaEnder.java
@@ -7,14 +7,23 @@
 package uk.me.fommil.beans;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import java.awt.Image;
 import java.beans.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Nullable;
 
 /**
  * Abstracts the JavaBeans API providing sensible actions and accessors.
  * Unless documented here, the JavaBeans API and surrounding ecosystem is
  * ignored.
+ * <p>
+ * In addition, the following listeners are managed:
  * <ul>
  * <li>{@link PropertyChangeListener}s - added via
  * {@link #addPropertyChangeListener(PropertyChangeListener)}
@@ -31,6 +40,8 @@ import javax.annotation.Nullable;
  * @see <a href="http://en.wikipedia.org/wiki/Bean_(Ender's_Game)">Bean and Ender</a>
  */
 public class JavaEnder {
+
+    private static final Logger log = Logger.getLogger(JavaEnder.class.getName());
 
     private final Object bean;
 
@@ -72,15 +83,117 @@ public class JavaEnder {
         return beaninfo.getIcon(iconKind);
     }
 
-    // TODO: abstractify so that we have nicer objects to work with
-    public PropertyDescriptor[] getPropertyDescriptors() {
+    /**
+     * A property held by a JavaBean
+     */
+    public class Property {
+
+        private final PropertyDescriptor descriptor;
+
+        private Property(PropertyDescriptor descriptor) {
+            Preconditions.checkNotNull(descriptor);
+            this.descriptor = descriptor;
+            Method getter = descriptor.getReadMethod();
+            Method setter = descriptor.getWriteMethod();
+            if (getter == null || getter.getParameterTypes().length != 0 || Modifier.isStatic(getter.getModifiers()) || !Modifier.isPublic(getter.getModifiers())) {
+                Preconditions.checkArgument(false, "bad getter");
+            }
+            if (setter == null || setter.getParameterTypes().length != 1 || Modifier.isStatic(setter.getModifiers()) || !Modifier.isPublic(setter.getModifiers())) {
+                Preconditions.checkArgument(false, "bad setter");
+            }
+        }
+
+        public Class<?> getPropertyClass() {
+            return descriptor.getPropertyType();
+        }
+
+        public Object getValue() {
+            Method method = descriptor.getReadMethod();
+            try {
+                return method.invoke(bean, new Object[0]);
+            } catch (Exception e) {
+                throw new RuntimeException(bean.getClass() + " doesn't support "
+                        + method.getName() + " as a JavaBean getter", e);
+            }
+        }
+
+        /**
+         * @param value a veto from a listener will result in a silent failure
+         */
+        public void setValue(Object value) {
+            Object old = getValue();
+            Method method = descriptor.getWriteMethod();
+            try {
+                vetoListeners.fireVetoableChange(getName(), old, value);
+            } catch (PropertyVetoException ex) {
+                log.info("Veto: " + bean.getClass() + "." + method.getName());
+                return;
+            }
+            try {
+                method.invoke(bean, value);
+                propListeners.firePropertyChange(getName(), old, value);
+            } catch (Exception e) {
+                throw new RuntimeException(bean.getClass() + " doesn't support "
+                        + method.getName() + " as a JavaBean setter", e);
+            }
+        }
+
+        // <editor-fold defaultstate="collapsed" desc="BOILERPLATE GETTERS/SETTERS">
+        public String getShortDescription() {
+            return descriptor.getShortDescription();
+        }
+
+        public String getDisplayName() {
+            return descriptor.getDisplayName();
+        }
+
+        public String getName() {
+            return descriptor.getName();
+        }
+
+        public boolean isHidden() {
+            return descriptor.isHidden();
+        }
+
+        public boolean isExpert() {
+            return descriptor.isExpert();
+        }
+        // </editor-fold>
+    }
+
+    /**
+     * @return the properties that are considered to be "bean like"
+     */
+    public List<Property> getProperties() {
+        PropertyDescriptor[] descriptors = getPropertyDescriptors();
+        if (descriptors == null || descriptors.length == 0) {
+            return Collections.emptyList();
+        }
+        List<Property> properties = Lists.newArrayList();
+        for (PropertyDescriptor descriptor : descriptors) {
+            if ("class".equals(descriptor.getName())) {
+                // crazy JavaBeans people
+                continue;
+            }
+            try {
+                Property property = new Property(descriptor);
+                properties.add(property);
+            } catch (IllegalArgumentException e) {
+                log.info("ignoring " + bean.getClass() + "." + descriptor.getName());
+            }
+        }
+        return properties;
+    }
+
+    private PropertyDescriptor[] getPropertyDescriptors() {
         PropertyDescriptor[] properties = beaninfo.getPropertyDescriptors();
         if (properties != null) {
             return properties;
         }
         try {
             BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
-            return beanInfo.getPropertyDescriptors();
+            properties = beanInfo.getPropertyDescriptors();
+            return properties;
         } catch (IntrospectionException ex) {
             throw new RuntimeException("Failed to access " + bean.getClass() + " as a JavaBean", ex);
         }
--- src/main/java/uk/me/fommil/beans/JavabeansEditorForm.java
+++ src/main/java/uk/me/fommil/beans/JavabeansEditorForm.java
@@ -7,10 +7,15 @@
 package uk.me.fommil.beans;
 
 import com.google.common.base.Preconditions;
-import java.awt.Component;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.beans.*;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import org.jdesktop.swingx.JXTable;
+import uk.me.fommil.beans.JavaEnder.Property;
 
 /**
  * An automatically-generated Swing Form for editing arbitrary objects
@@ -29,40 +34,116 @@ import javax.swing.JPanel;
  * {@link BeanInfo}.
  * <p>
  * If the JavaBean implements {@link BeanInfo}, or one is provided
- * through {@link #setEnder(JavaEnder)}, then it will be used.
- * <p>
- * Features:
+ * through {@link #setEnder(JavaEnder)}, then it will be used for the following:
  * <ul>
  * <li>{@link BeanInfo#getIcon(int)} is used, if available.</li>
  * <li>{@link PropertyDescriptor#isHidden()} is respected.</li>
  * <li>{@link PropertyDescriptor#isExpert()} will produce a read-only entry
  * (a useful interpretation of a vague API).</li>
  * </ul>
- * TODO: Convenience methods are provided to make use of the features.
- * <p>
- * The latter two features provide a simple alternative to registering listeners
- * directly with the JavaBeans.
+ * TODO: Convenience methods are provided to make it easy to use these features.
  * <p>
  * This is not capable of detecting changes made to the
- * underlying bean by others, so a call to {@link #revalidate()} is recommended
+ * underlying bean by others, so a call to {@link #refresh()} is recommended
  * if changes are made.
  * 
  * @see <a href="http://stackoverflow.com/questions/10840078">Question on Stack Overflow</a>
  * @author Samuel Halliday
  */
-public class JavabeansEditorForm extends JComponent {
+public final class JavabeansEditorForm extends JPanel {
 
+    private static final Logger log = Logger.getLogger(JavabeansEditorForm.class.getName());
+
+    /** @param args */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JavabeansEditorForm editor = new JavabeansEditorForm();
+        JavaEnder ender = new JavaEnder(new Object() {
+
+            private String name = "test";
+
+            // <editor-fold defaultstate="collapsed" desc="BOILERPLATE GETTERS/SETTERS">
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+            // </editor-fold>
+        });
+        ender.addPropertyChangeListener(new PropertyChangeListener() {
+
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                log.info("Received Change: " + evt.getNewValue());
+            }
+        });
+        ender.addVetoableChangeListener(new VetoableChangeListener() {
+
+            @Override
+            public void vetoableChange(PropertyChangeEvent evt) throws PropertyVetoException {
+                log.info("Received VetoableChange: " + evt.getNewValue());
+                throw new PropertyVetoException("No", evt);
+            }
+        });
+
+        editor.setEnder(ender);
+        frame.add(editor, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
     private volatile JavaEnder ender;
+
+    private final JXTable table = new JXTable();
 
     public JavabeansEditorForm() {
         super();
+        setLayout(new FlowLayout());
+
+        table.setTableHeader(null);
+
+        JScrollPane scroll = new JScrollPane(table);
+        add(scroll);
     }
 
-    @Override
-    public void revalidate() {
-        // TODO: update fields
+    public void refresh() {
+        final List<Property> properties = ender.getProperties();
 
-        super.revalidate();
+        table.setModel(new AbstractTableModel() {
+
+            @Override
+            public int getRowCount() {
+                return properties.size();
+            }
+
+            @Override
+            public int getColumnCount() {
+                return 2;
+            }
+
+            @Override
+            public Object getValueAt(int rowIndex, int columnIndex) {
+                Preconditions.checkArgument(rowIndex >= 0 && rowIndex < getRowCount());
+                Preconditions.checkArgument(columnIndex >= 0 && columnIndex < getColumnCount());
+                switch(columnIndex) {
+                    case 0: return properties.get(rowIndex).getName();
+                    default: return properties.get(rowIndex).getValue();
+                }
+            }
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                Preconditions.checkArgument(rowIndex >= 0 && rowIndex < getRowCount());
+                if (columnIndex == 0) {
+                    return false;
+                }
+                return !properties.get(rowIndex).isExpert();
+            }
+        });
+
+        invalidate();
     }
 
     /**
@@ -79,5 +160,6 @@ public class JavabeansEditorForm extends JComponent {
     public void setEnder(JavaEnder ender) {
         Preconditions.checkNotNull(ender);
         this.ender = ender;
+        refresh();
     }
 }
